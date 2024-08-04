9afc96539ae307acb00e7352c736cb23c1245781
mai <maicranberry669@gmail.com>
2024-08-04 11:11:10 +0900

    サンクスページのviewファイル作成

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/thanks.css') }}">
+@endsection
+
+@section('content')
+<div class="thanks__content">
+    <div class="thanks__heading">
+        会員登録ありがとうございます
+    </div>
+    <div class="login__button">
+        <a class="login__button-link" href="/login">ログインする</a>
+    </div>
+</div>
+@endsection
