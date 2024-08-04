295158c67db0d8992d79ee1162ca4b90bba4f84a
mai <maicranberry669@gmail.com>
2024-08-04 11:14:51 +0900

    予約完了ページのviewファイル作成

---
+++ src/resources/views/done.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/done.css') }}">
+@endsection
+
+@section('content')
+<div class="thanks__content">
+    <div class="thanks__heading">
+        ご予約ありがとうございます
+    </div>
+    <div class="back__button">
+        <a class="back__button-link" href="/">戻る</a>
+    </div>
+</div>
+@endsection
